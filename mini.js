ed3cf0353110575bcdfb0d5291dd29cc17e4343a
Student-rutujaG <rutujaganje39@gmail.com>
2025-12-17 15:28:34 +0530

    Added Android click support and fixed game reset logic

--- mini.js
+++ mini.js
@@ -70,7 +70,11 @@ function checkSqc(idx) {
     } else {
         if (/Mobi|Android/i.test(navigator.userAgent)) {
             h2.innerText = "Game Over! Tap anywhere to restart";
-           reset();
+             document.querySelector("body").style.backgroundColor = "red";
+            setTimeout(() => {
+                document.querySelector("body").style.backgroundColor = "white";
+            }, 150);
+            reset();
         } else {
             h2.innerText = "Game Over! Press any key to restart";
             document.querySelector("body").style.backgroundColor = "red";
