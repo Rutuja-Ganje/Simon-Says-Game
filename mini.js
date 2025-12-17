a7d5ed6c2995c7ecbb4b035a0a0f1f8e4a8d3859
Student-rutujaG <rutujaganje39@gmail.com>
2025-12-17 14:55:27 +0530

    Added Android click support and fixed game reset logic

--- mini.js
+++ mini.js
@@ -2,7 +2,7 @@ let gameSqc = [];
 let userSqc = [];
 let started = false;
 let level = 0;
-let btns=["yellow","red","purple","green"];
+let btns = ["yellow", "red", "purple", "green"];
 let h2 = document.querySelector("h2");
 // let isMobile=/Mobi|Anroid/i.test(navigator.userAgent);
 // document.addEventListener("keypress", function (e) {
@@ -16,86 +16,92 @@ let h2 = document.querySelector("h2");
 // })
 
 if (/Mobi|Android/i.test(navigator.userAgent)) {
-  // Mobile: tap anywhere
-  document.addEventListener("click", function () {
-    if (!started) {
-      started = true;
-      levelUp();
-    }
-  });
+    // Mobile: tap anywhere
+    document.addEventListener("click", function () {
+        if (!started) {
+            started = true;
+            levelUp();
+        }
+    });
 } else {
-  // Desktop: press any key
-  document.addEventListener("keypress", function () {
-    if (!started) {
-      started = true;
-      levelUp();
-    }
-  });
+    // Desktop: press any key
+    document.addEventListener("keypress", function () {
+        if (!started) {
+            started = true;
+            levelUp();
+        }
+    });
 }
 
 function btnFlash(btn) {
     btn.classList.add("flash");
-    setTimeout(function(){
+    setTimeout(function () {
         btn.classList.remove("flash");
-    },250);
+    }, 250);
 }
 
 function levelUp() {
-    userSqc=[];
+    userSqc = [];
     level++;
     h2.innerText = `Level ${level}`;
-    let randIdx=Math.floor(Math.random()*3);
-    let randomColor=btns[randIdx];
-    let randbtn=document.querySelector(`.${randomColor}`);
+    let randIdx = Math.floor(Math.random() * 3);
+    let randomColor = btns[randIdx];
+    let randbtn = document.querySelector(`.${randomColor}`);
 
     // console.log(randIdx);
-   
+
     // console.log(randomColor);
     //  console.log(randbtn);
     gameSqc.push(randomColor);
     console.log(gameSqc);
-    
-    
+
+
     btnFlash(randbtn);
 }
 
-function checkSqc(idx){
+function checkSqc(idx) {
     console.log(level);
-   
-    if(userSqc[idx]==gameSqc[idx]){
-       if(userSqc.length==gameSqc.length){
-        setTimeout(levelUp, 1000);
-       }
-        
-    }else{
-        h2.innerHTML=`Game Over! Your Score was <b>${level}</b><br>Press any key  to start`;
-        document.querySelector("body").style.backgroundColor="red";
-        setTimeout(() => {
-            document.querySelector("body").style.backgroundColor="white";
-        }, 150);
-        reset();
+
+    if (userSqc[idx] == gameSqc[idx]) {
+        if (userSqc.length == gameSqc.length) {
+            setTimeout(levelUp, 1000);
+        }
+
+    } else {
+        if (/Mobi|Android/i.test(navigator.userAgent)) {
+            h2.innerText = "Game Over! Tap anywhere to restart";
+        } else {
+            h2.innerText = "Game Over! Press any key to restart";
+        }
+
+        // h2.innerHTML=`Game Over! Your Score was <b>${level}</b><br>Press any key  to start`;
+        // document.querySelector("body").style.backgroundColor="red";
+        // setTimeout(() => {
+        //     document.querySelector("body").style.backgroundColor="white";
+        // }, 150);
+        // reset();
     }
 }
 
-function btnPress(){
-    let clickedBtn=this;
+function btnPress() {
+    let clickedBtn = this;
     btnFlash(clickedBtn);
 
-    let userColor=clickedBtn.getAttribute("id");
+    let userColor = clickedBtn.getAttribute("id");
     // console.log(userColor);
     userSqc.push(userColor);
-    checkSqc(userSqc.length-1);
-    
-    
+    checkSqc(userSqc.length - 1);
+
+
 }
-let allBtns=document.querySelectorAll(".btn");
-for(btn of allBtns){
-    btn.addEventListener("click",btnPress);
+let allBtns = document.querySelectorAll(".btn");
+for (btn of allBtns) {
+    btn.addEventListener("click", btnPress);
 }
-function reset(){
-    started=false;
-    gameSqc=[];
-    userSqc=[];
-    level=0;
+function reset() {
+    started = false;
+    gameSqc = [];
+    userSqc = [];
+    level = 0;
 
 }
